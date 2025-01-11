77cf62aa83fd88723db5e41d40531f087d9972f9
ZukaAlavidze <zuka@alavidze.com>
2025-01-11 16:54:08 -0500

    try 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/food-trucking/',
+  base: 'zukaalavidze/food-trucking/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
