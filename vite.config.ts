6c9a130b066b932d5187b53fbc70af7ddc948f28
ZukaAlavidze <zuka@alavidze.com>
2025-01-11 20:22:23 -0500

    updated github deployment configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://zukaalavidze.github.io/food-trucking/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  base: '/food-trucking/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
   },
-});
+})
